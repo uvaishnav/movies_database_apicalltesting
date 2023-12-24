74ce5a8557428179ae19c78d7468957db2ba1a8a
uvaishnav <vuppalap@gitam.in>
2023-12-24 17:26:52 +0000

    configured AP1 and corrected some errors in initialising db

--- app.js
+++ app.js
@@ -3,27 +3,45 @@ const app = express();
 
 const path = require("path");
 
-const {open} = require("sqlite");
+const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 
 const dbPath = path.join(__dirname, "moviesData.db");
 
 let db = null;
 
-const initializeDbAndServer = async () =>{
-    try{
-        db = await open({
-            path = dbPath;
-            driver = sqlite3.Database;
-        });
-        app.listen(3000,()=>{
-            console.log("Server listening at Port 3000");
-        })
-    }catch(e){
-        console.log(`DB Error : ${e.message}`);
-        process.exit(1);
-    }
+const initializeDbAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server listening at Port 3000");
+    });
+  } catch (e) {
+    console.log(`DB Error : ${e.message}`);
+    process.exit(1);
+  }
 };
 
 initializeDbAndServer();
 
+// API 1
+
+const convertDbObjToResponseObj1 = (dbObj) => {
+  return {
+    movieName: dbObj.movie_name,
+  };
+};
+
+app.get("/movies/", async (request, response) => {
+  const getAllMovieNamesQuery = `
+    select movie_name from movie order by movie_id;`;
+
+  const allMovieArray = await db.all(getAllMovieNamesQuery);
+  const responseMovieListArray = allMovieArray.map((eachMovie) => {
+    return convertDbObjToResponseObj1(eachMovie);
+  });
+  response.send(responseMovieListArray);
+});
